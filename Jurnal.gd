ea967fa008f5ae1cc82e7ca7cc5d241f90ac9d1e
PurpleHamster1 <alexbarbu100@yahoo.com>
2024-03-24 18:22:51 +0200

    jurnal

---
+++ Jurnal.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var buttonsContainer = $List/VBoxContainer
+@onready var descContainer = $Desc
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for node in buttonsContainer.get_children():
+		node.pressed.connect(changeDesc.bind(node.name))
+		
+
+func changeDesc(node):
+	for v in descContainer.get_children():
+		v.visible = false
+	descContainer.find_child(node).visible = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
